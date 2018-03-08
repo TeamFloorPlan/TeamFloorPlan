d5ffc42abd752d769b489b49b658e8d1c273532f
MBew <up804546@myport.ac.uk>
2018-03-08 05:04:14 +0000

    Added comments to JS file

--- mapScript.js
+++ mapScript.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*a function to draw the map to the screen using the canvas module*/
 function displayMap(el) {
   var c = el.getContext("2d");
   c.clearRect(0, 0, 500, 350);
@@ -9,36 +10,37 @@ function displayMap(el) {
   var img = new Image();
   img.src = getBuilding();
   img.onload = function() {
+    /*Draws image onto canvas size 500px by 350px*/
     c.drawImage(img, 0, 0, 500, 350)
   }
 }
 
+/*Function to determine which building and which floor to display*/
 function getBuilding() {
-
+  /*Gets input of building name from text entry box*/
   switch (document.getElementById('buildingVal').value) {
     case "liongate":
+    /*Gets input of floor number from spinner*/
       switch (document.getElementById('floor').value) {
         case "0":
-          var build="img/Liongate/liongateLevel0.svg"
+          var build="img/liongateLevel0.svg"
           break;
         case "1":
-          var build="img/Liongate/liongateLevel1.svg"
-          break;
-        case "2";
-          var build="img/Liongate/liongateLevel2.svg"
+          var build="img/liongateLevel1.svg"
           break;
       }
       break;
     case "portland":
       switch (document.getElementById('floor').value) {
         case "0":
-          var build="img/Portland/portlandLevel0.svg"
+          var build="img/portlandLevel0.svg"
           break;
         case "1":
-          var build="img/Portland/portlandLevel0.svg"
+          var build="img/portlandLevel0.svg"
           break;
         }
       break;
   }
+  /*Returns the location of the appropriate image*/
   return build;
 }
