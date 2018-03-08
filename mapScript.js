27821f94140501afdccf17e53f8fd3756529e51a
MBew <UP804546@uni.ds.port.ac.uk>
2018-03-08 01:49:22 +0000

    Added map floor selection and drawing using canvas

--- mapScript.js
+++ mapScript.js
@@ -1,34 +1,41 @@
 'use strict';
 
-function toRichmondLT(el) {
+function displayMap(el) {
   var c = el.getContext("2d");
+  c.clearRect(0, 0, 500, 350);
   c.lineWidth = 5;
   c.strokeStyle = "red";
   c.lineCap = "round";
   var img = new Image();
-  img.src = 'LT1testMap.png';
+  img.src = getBuilding();
   img.onload = function() {
-    c.drawImage(img, 0, 0)
-    c.beginPath();
-    c.moveTo(35,260);
-    c.lineTo(180,230);
-    c.stroke();
-
-    c.beginPath();
-    c.moveTo(180,230);
-    c.lineTo(210,230);
-    c.stroke();
-
-    c.beginPath();
-    c.moveTo(210,230);
-    c.lineTo(210,295);
-    c.stroke();
-
-    c.beginPath();
-    c.moveTo(210,295);
-    c.lineTo(240,295);
-    c.stroke();
-
-
+    c.drawImage(img, 0, 0, 500, 350)
   }
+}
+
+function getBuilding() {
+
+  switch (document.getElementById('buildingVal').value) {
+    case "liongate":
+      switch (document.getElementById('floor').value) {
+        case "0":
+          var build="img/liongateLevel0.svg"
+          break;
+        case "1":
+          var build="img/liongateLevel1.svg"
+          break;
+      }
+      break;
+    case "portland":
+      switch (document.getElementById('floor').value) {
+        case "0":
+          var build="img/portlandLevel0.svg"
+          break;
+        case "1":
+          var build="img/portlandLevel0.svg"
+          break;
+        }
+      break;
+  }
+  return build;
 }
