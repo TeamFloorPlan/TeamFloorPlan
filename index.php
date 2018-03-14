c76984f006338db02c6b7e404cbd41144a75c553
kevinli532 <UP839573@uni.ds.port.ac.uk>
2018-03-14 17:20:01 +0000

    testing login on php rather than html pages

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="icon" href="img/UOPLogos/UOPLogoSmall.PNG">
+    <link rel="stylesheet" href="style/stylesheet.css">
+    <script src="mapScript.js"></script>
+  </head>
+  <header>
+    <a href="index.html"><img id="logo" src="img/UOPLogos/roomFinderUOPLogo.png"
+     alt="Logo"></a>
+    <h1>Homepage</h1>
+      <nav>
+        <a id="current_page">Home</a> |
+        <a href="login.html" aria-label="Navigate to the sign in page">Login</a> |
+        <a href="buildingSelect.html" aria-label="Navigate to the building select page">Building Select</a> |
+        <a href="settings.html" aria-label="Navigate to the settings page">Settings</a> |
+      </nav>
+  </header>
+  <body>
+
+    <section>
+      <div id="homeBanner">
+        <p id="bannerMain">UoP Room Finder</p>
+        <p id="bannerSub">INSE Project</p>
+        <a href="login.html"><img id="loginBtnHome" src="img/homeLogin.png" width="180px"></a>
+      </div>
+      <div id="homeInfo">
+        <h2>Why Sign Up?</h2>
+        <p>The 'University Of Portsmouth Room Finder' is a web application designed to help you on your educational journey through life. By signing up you will gain access to a system designed by and for students to get you from A to B in your first days on campus.</p>
+        <p>It's as simple as entering your building, floor and room number. Our world class mapping algorithm will guide you to your classroom, avoiding that first week panic.</p>.
+        <img src="img/lostStudent.png" width="90%">
+      </div>
+    </section>
+
+  </body>
+</html>
---
+++ passwrong.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Create An Account</title>
+    <link rel="icon" href="img/UOPLogos/UOPLogoSmall.PNG">
+    <link rel="stylesheet" href="style/stylesheet.css">
+  </head>
+  <header>
+    <a href="index.html"><img id="logo" src="img/UOPLogos/roomFinderUOPLogo.PNG"
+     alt="Logo"></a>
+    <h1>Homepagez</h1>
+      <nav>
+        <a href="index.html" aria-label="Navigate to the start page">Home</a> |
+        <a href="login.html">Login</a> |
+      </nav>
+  </header>
+  <body>
+    <article>
+
+    </article>
+  </body>
+</html>
